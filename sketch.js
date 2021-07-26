a964ccc85209c549414916ff2486045b2728d48e
Yash Jain <76769621+yashjain7856@users.noreply.github.com>
2021-07-27 01:01:00 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 let blackJet;
 let whiteJet;
-
+let timer=30;
 let blackJetImage;
 let whiteJetImage;
 const ROTATE_AMOUNT = 0.05;
@@ -26,16 +26,38 @@ function draw() {
   
   blackJet.draw();
   whiteJet.draw(); 
-  
-  
+    
   textSize(80);
   fill(0);
-  text(blackJet.score, 0, 70);
+  text(blackJet.score, 10, 70);
   
   fill(255);
   text(whiteJet.score, 350, 70);
-}
 
+  textSize(50);
+  fill(255);
+  text(timer, 210, 490);
+  if(timer<=0)
+  {
+    noLoop();
+    textSize(50);
+    fill(255);
+    text("Game Over", 100, 250);
+    if(whiteJet.score>blackJet.score)
+    {
+      text("White Wins", 100, 350);
+    }
+    else if (whiteJet.score<blackJet.score)
+    {
+      text("Black Wins", 100, 350);
+    }
+    else 
+    {
+      text("It's a tie!", 100, 350);
+    }
+  }
+}
+setInterval(function(){ timer = timer-1; },1000);
 
 function keyPressed() {
 	if (keyCode === RIGHT_ARROW) {
